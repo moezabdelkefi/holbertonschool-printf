f5470e66f545db207ea5c2245575d3d582f7da8b
root <root@DESKTOP-C0MK0L2.localdomain>
2022-11-12 08:12:45 -0930

    fix betty

--- print_digit.c
+++ print_digit.c
@@ -7,29 +7,29 @@
 int printf_digit(va_list arg)
 {
 	int n, m = 1, c = 0;
-	unsigned int s;
+	unsigned int k;
 
 	n = va_arg(arg, int);
 
 	if (n < 0)
 	{
 		putchar('-');
-		s = n * -1;
+		k = n * -1;
 		c++;
 	}
 	else
 	{
-		s = n;
+		k = n;
 	}
 
-	while ((s / m) >= 10)
+	while ((k / m) >= 10)
 	{
 		m = m * 10;
 	}
 	while (m != 0)
 	{
-		c = c + putchar(s / m + '0');
-		s = s % m;
+		c = c + putchar(k / m + '0');
+		k = k % m;
 		m = m / 10;
 	}
 	return (c);
