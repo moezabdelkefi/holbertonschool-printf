98377415490a0eaade9d21bedce02e45010c51bc
root <root@DESKTOP-C0MK0L2.localdomain>
2022-11-13 13:50:50 -0930

    print

--- printf_match.c
+++ printf_match.c
@@ -16,11 +16,13 @@ int print_match(const char *format, print pt[], va_list args)
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
-		{ /*begin when we found % and begin we found the fisrt character after percent*/
+		{ /*begin when we found % */
+			/*and begin we found the fisrt character after percent*/
 			for (j = 0; pt[j].data != NULL; j++)
 			{
 				if (format[i + 1] == pt[j].data[0])
-				{ /* search about the character with the struct "pt" and incriment the counter j*/
+				{ /* search about the character*/
+					/* with the struct "pt" and incriment the counter j*/
 					val = pt[j].y(args);
 					if (val == -1)
 						return (-1);
@@ -31,7 +33,8 @@ int print_match(const char *format, print pt[], va_list args)
 			if (pt[j].data == NULL && format[i + 1] != ' ')
 			{ /*begin when the character dosent existe in "pt"*/
 				if (format[i + 1] != '\0')
-				{ /*print the character percent and after character percent and incriment c with 2 character*/
+				{ /*print the character percent*/
+					/*and after character percent and incriment c with 2 character*/
 					_putchar(format[i]);
 					_putchar(format[i + 1]);
 					count = count + 2;
