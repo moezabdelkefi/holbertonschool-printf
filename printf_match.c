4c3942842929e3ed5063a34777f87f3dec538e69
root <root@DESKTOP-C0MK0L2.localdomain>
2022-11-14 00:27:54 -0930

    printf

--- printf_match.c
+++ printf_match.c
@@ -10,42 +10,44 @@
  */
 int print_match(const char *format, print pt[], va_list args)
 {
-	int i, s, val;
-	int count = 0;
 
-	for (i = 0; format[i] != '\0'; i++)
+	int i = 0, j, k = 0, done = 0;
+	int c = 0;
+
+	for (i = 0; format && format[i] != 0; i++)
 	{
-		if (format[i] == '%')
+		if (format[i] != '%')
 		{
-			for (s = 0; pt[s].data != NULL; s++)
+			_putchar(format[i]);
+			c = c + 1;
+		}
+		else
+		{
+			for (j = 0; pt[j].data; j++)
 			{
-				if (format[i + 1] == pt[s].data[0])
+				if (format[i + 1] == pt[j].data[k])
 				{
-					val = pt[s].y(args);
-					if (val == -1)
-						return (-1);
-					count += val;
+					done = pt[j].y(args);
+					c += done;
+					i++;
 					break;
 				}
 			}
-			if (pt[s].data == NULL && format[i + 1] != ' ')
+			if (pt[j].data == NULL && format[i + 1] != ' ')
 			{
-				if (format[i + 1] != '\0')
+				if (format[i + 1] != 0)
 				{
-					_putchar(format[i]);
-					_putchar(format[i + 1]);
-					count = count + 2;
+					putchar(format[i]);
+					putchar(format[i + 1]);
+					c = c + 2;
+					i++;
 				}
 				else
 					return (-1);
 			}
-			i = i + 1;
-		}
-		else
-		{
-			_putchar(format[i]);
-			count++;
 		}
 	}
-	return (count);
+	if (format == NULL)
+		return (-1);
+	return (c);
 }
